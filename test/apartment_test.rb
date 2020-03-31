fceb802b41c9ffa0bfa5fc8fa9b112f9399ba621
Alex Pariseau <arpariseau@gmail.com>
2020-03-31 09:24:13 -0600

    Created apartment instancing

---
+++ test/apartment_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/renter'
+require './lib/apartment'
+
+class ApartmentTest < Minitest::Test
+
+  def test_existence
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_instance_of Apartment, unit1
+  end
+
+  def test_attribute_access
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    assert_equal "A1", unit1.number
+    assert_equal 1200, unit1.monthly_rent
+    assert_equal 1, unit1.bathrooms
+    assert_equal 1, unit1.bedrooms
+    assert_nil unit1.renter
+  end
+  
+end
